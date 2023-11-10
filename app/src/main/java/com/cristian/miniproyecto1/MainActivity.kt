1140c9bb4a77ce250f6029e1c3e0e2ed46756c45
Invitado <invitado@s1pc28.eisc.univalle.edu.co>
2023-11-10 16:51:46 -0500

    HU2 Botón parpadeante y sonido de fondo. HU2 completada

--- app/src/main/java/com/cristian/miniproyecto1/MainActivity.kt
+++ app/src/main/java/com/cristian/miniproyecto1/MainActivity.kt
@@ -3,12 +3,19 @@ package com.cristian.miniproyecto1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.ScaleAnimation
+import android.widget.Button
 import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import com.cristian.miniproyecto1.databinding.ActivityMainBinding
+import android.media.MediaPlayer
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
+    private lateinit var circularButton: Button
+    private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +23,18 @@ class MainActivity : AppCompatActivity() {
 
         val shareButton: ImageView = binding.toolbar.shareButton
 
+        circularButton = binding.button
+        startRippleAnimation(circularButton)
+
         shareButton.setOnClickListener {
             share()
         }
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.bgsound)
+
+        mediaPlayer.isLooping = true
+        mediaPlayer.start()
+        mediaPlayer.setVolume(1.0f, 1.0f)
 
     }
 
@@ -30,6 +46,35 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra(Intent.EXTRA_SUBJECT, shareSub)
         intent.putExtra(Intent.EXTRA_TEXT, shareBody)
         startActivity(Intent.createChooser(intent, "Share using"))
+    }
+
+    private fun startRippleAnimation(view: Button) {
+        val animationSet = AnimationSet(true)
+
+        for (i in 1..3) {
+            val scaleAnimation = ScaleAnimation(
+                0f,
+                1f,
+                0f,
+                1f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f
+            )
+            scaleAnimation.duration = 500
+            scaleAnimation.fillAfter = true
+            scaleAnimation.repeatCount = Animation.INFINITE
+
+            animationSet.addAnimation(scaleAnimation)
+        }
+
+        view.startAnimation(animationSet)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
     }
 
 
