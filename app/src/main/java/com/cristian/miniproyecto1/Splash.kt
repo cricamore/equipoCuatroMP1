494b4db5e15bf2c8d5ad2ccb6fd137d2d48dee61
lauramoyano <laura.moyano.go@gmail.com>
2023-11-10 19:03:18 -0500

    animación

--- app/src/main/java/com/cristian/miniproyecto1/Splash.kt
+++ app/src/main/java/com/cristian/miniproyecto1/Splash.kt
@@ -1,11 +1,23 @@
 package com.cristian.miniproyecto1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.animation.LinearInterpolator
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.splash)
+
+        val iconImageView = findViewById<ImageView>(R.id.iconImageView)
+        val rotation = ObjectAnimator.ofFloat(iconImageView, "rotation", 0f, 360f)
+        rotation.duration = 2000
+
+        rotation.interpolator = LinearInterpolator()
+        rotation.repeatCount = ObjectAnimator.INFINITE
+        rotation.start()
     }
 }
