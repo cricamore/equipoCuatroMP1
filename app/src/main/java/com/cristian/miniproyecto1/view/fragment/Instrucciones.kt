534ed863d0b4f42f76b00c06ed24faabbd33b8ad
rwxp <sdrivert@hotmail.com>
2023-11-14 12:36:57 -0500

    Animacion agregada

--- app/src/main/java/com/cristian/miniproyecto1/view/fragment/Instrucciones.kt
+++ app/src/main/java/com/cristian/miniproyecto1/view/fragment/Instrucciones.kt
@@ -1,5 +1,6 @@
 package com.cristian.miniproyecto1.view.fragment
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -24,10 +25,19 @@ class Instrucciones : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         controllers()
+        animate()
     }
     fun controllers(){
         binding.btnBack.setOnClickListener{
             findNavController().navigate(R.id.action_to_principal)
         }
+
+    }
+
+    private fun animate() {
+        val rotationAnimator = ObjectAnimator.ofFloat(binding.ivPersona, "rotation", 0f, 360f)
+        rotationAnimator.duration = 1000
+        rotationAnimator.repeatCount = ObjectAnimator.INFINITE
+        rotationAnimator.start()
     }
 }
